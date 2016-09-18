457291b2afc0dd1e76741bae42408debff6aa3fc
minal <minalc5695@gmail.com>
2016-09-18 11:40:58 +0530

    first program of python in PythonTutor

---
+++ sample3.py
@@ -0,0 +1,11 @@
+__author__='minal'
+from random import randint
+def first_function():
+    message=raw_input("please enter something")
+    print type(message)
+    print('your message:',message)
+    print('length:',len(message))
+
+if __name__ == "__main__":
+     first_function()
+
